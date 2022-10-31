82723bd98093d2d97e577e3387bc75c1b11aa15c
Peter Kim <peter.kim@uconn.edu>
2022-10-31 01:19:31 -0400

    mysql connection made

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const mysql = require('mysql2');
+
+// makes a connection to the mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'employees_db'
+    },
+    console.log("Connected to Employees_db")
+);
